08775f67440103c32537d29b6bad3581f846191e
robert <robert@urban4m.com>
2015-04-25 16:33:46 -0400

    provide pure sync.WaitGroup example for comparison.

--- ex/main.go
+++ ex/main.go
@@ -2,19 +2,43 @@ package main
 
 import (
 	"log"
+	"math/rand"
 	"runtime"
+	"sync"
 	"time"
 
 	workgroup "github.com/Urban4M/go-workgroup"
 )
 
-// A workgroup example.
+// A comparison of workgroup vs pure sync.WaitGroup
 func main() {
+
+	workers := runtime.NumCPU()
+	runtime.GOMAXPROCS(workers)
+
+	generate := 100
+
+	group := sync.WaitGroup{}
+	group.Add(2)
+	go func() {
+		usingWorkgroup(workers, generate)
+		group.Done()
+	}()
+	go func() {
+		usingWaitGroup(workers, generate)
+		group.Done()
+	}()
+	group.Wait()
+}
+
+// The workgroup version
+func usingWorkgroup(workers, generate int) {
 
 	// This is a Worker function. The workgroup will start however many of these
 	// you specify. In this example, it will start one for each CPU (see below).
 	fanOutWorker := func(worker int, work workgroup.Work) {
-		log.Printf("%d %+v Done : +%v", worker, work, time.Now())
+		time.Sleep(time.Duration(rand.Int63n(1000)) * time.Millisecond)
+		log.Printf("workgroup: worker %d handing work %d.", worker, work)
 	}
 
 	// This is a Work-Generator. It simply feeds work to each Worker goroutine
@@ -24,13 +48,10 @@ func main() {
 	// The completion of this signals the workgroup's cleanup process (all the
 	// Workers will complete their work.)
 	workUnits := workgroup.Generator(func(out chan<- workgroup.Work) {
-		for i := 0; i < 100; i++ {
+		for i := 0; i < generate; i++ {
 			out <- i
 		}
 	})
-
-	workers := runtime.NumCPU()
-	runtime.GOMAXPROCS(workers)
 
 	// This configures and initates the workgroup.
 	// FanOut specifies how many Workers to start.
@@ -39,5 +60,45 @@ func main() {
 	workgroup.FanOut(workers).Drain(workUnits).With(fanOutWorker).Go()
 
 	// This is just to show that Go blocks.
+	log.Printf("Done: elapsed:+%v", time.Now())
+
+}
+
+// The equivolent sync.WaitGroup version.
+func usingWaitGroup(workers, generate int) {
+
+	var wg sync.WaitGroup
+
+	worker := func(worker int) func(in <-chan int) {
+		return func(in <-chan int) {
+			go func() {
+				defer wg.Done()
+
+				// This is essentially the workgroup Worker function.
+				for work := range in {
+					time.Sleep(time.Duration(rand.Int63n(1000)) * time.Millisecond)
+					log.Printf("WaitGroup: worker %d handing work %d.", worker, work)
+				}
+			}()
+		}
+	}
+
+	in := make(chan int)
+
+	wg.Add(workers)
+	for w := 0; w < workers; w++ {
+		go worker(w)(in)
+	}
+
+	// This is essentially the workgroup Generator function.
+	for w := 0; w < generate; w++ {
+		in <- w
+	}
+
+	close(in)
+
+	// This is essentially the workgroup Go() call.
+	wg.Wait()
+
 	log.Printf("Done: elapsed:+%v", time.Now())
 }
